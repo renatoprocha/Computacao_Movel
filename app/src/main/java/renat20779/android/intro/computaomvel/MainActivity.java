766fc03b152f4b86e2ab688263689e9384455cd5
Renato Rocha <renato.pr99@gmail.com>
2020-03-09 21:49:47 +0000

    Initial commit

---
+++ app/src/main/java/renat20779/android/intro/computaomvel/MainActivity.java
@@ -0,0 +1,14 @@
+package renat20779.android.intro.computaomvel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/renat20779/android/intro/computaomvel/NoteDB.java
@@ -0,0 +1,16 @@
+package renat20779.android.intro.computaomvel;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NoteDB extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
